8d93a0208c36d0d71858274c2b0accaac4dfca42
Manssuresh <manssuresh0421@gmail.com>
2023-07-25 18:57:23 +0530

    corrected typo

--- userdata .sh
+++ userdata .sh
@@ -18,7 +18,7 @@ unzip simply-amazed.zip
 rm -rf simply-amazed.zip 
 cd 2123_simply_amazed
 #backslash before cp tp avoid prompt for overwrite
-\cp -rf * /usr/share//nginx/html/nginx
+\cp -rf * /usr/share/nginx/html/nginx
 
 #INSTALL TOMCAT
 #!/bin/bash
